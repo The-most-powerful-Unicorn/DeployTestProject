52eded16981adb10702d1f4f772b1b217ee4bf4b
Hyun <itsmine096@gmail.com>
2022-08-19 09:17:56 +0900

    프로젝트 생성

---
+++ src/main/java/com/tmpu/deployTestProject/DeployTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tmpu.deployTestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployTestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmpu/deployTestProject/DeployTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmpu.deployTestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
